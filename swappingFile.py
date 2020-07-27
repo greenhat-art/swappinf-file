2f3b1c2dbed896769d1ca2f3cec954bdc4218cb3
greenhat-art <60651868+greenhat-art@users.noreply.github.com>
2020-07-27 16:14:21 +0530

    Add files via upload

---
+++ swappingFile.py
@@ -0,0 +1,17 @@
+def SwapFileData():
+    fileOneData=input("Enter your file name :  ")
+    fileTwoData=input("Enter your file name :  ")
+    
+    with open(fileOneData,'r') as sampleA:
+        data_sampleA=sampleA.read()
+
+    with open(fileTwoData, 'r') as sampleB:
+        data_sampleB=sampleB.read()
+
+    with open(fileOneData, 'w') as sampleA:
+        sampleA.write(data_sampleB)
+
+    with open(fileTwoData, 'w') as sampleB:
+        sampleB.write(data_sampleA)
+
+SwapFileData()
